0ae5cab2cbea46c48190223c3242e82dec8fe70b
talipbozkurt <talipbozkurt19@gmail.com>
2025-04-26 10:34:27 +0300

    gitignore updated

---
+++ homeworks/src/main/kotlin/Main.kt
@@ -0,0 +1,108 @@
+package org.example
+
+
+fun main() {
+    fun main() {
+        val sehir = "İzmir"
+        val ulke = "Türkiye"
+        val telefon = "0555 000 00 00"
+        val postaKodu = "34545"
+        val email = "example@gmail.com"
+        val meslek = "Mühendis"
+        val stokMiktari = 120855
+        val musteriAdi = "Hakkı BULUT"
+        val bakiye = 1545
+        val dogumGunu = "01.01.2000"
+        val maas = 18700.00
+        val medeniDurum = "Evli"
+        val urunYorum = "Çok kaliteli bir ürün."
+
+        val odemeTarihi = "26.04.2025"
+        val odeme = 300.00
+        val siparisAdeti = 3
+        val arabaModeli = "BMW 320i"
+        val kitapAdi = "Suç ve Ceza"
+        val yayinlamaTarihi = "1987"
+        val indirimMiktari = 21.00
+        val odaSayisi = 4
+        val enlem = 41.0082
+        val boylam = 28.9784
+        val urunAdi = "Kalem"
+        val yemekFiyati = 180.00
+
+        val marka = "Apple"
+        val muzikAdi = "Bohemian Rhapsody"
+        val videoSuresi = "5 dakika 55 saniye"
+        val urunPuani = 4.8
+        val resimAdi = "manzara.jpg"
+        val dosyaFormati = "JPEG"
+        val renk = "Mavi"
+        val renkKodu = "#0000FF"
+        val telefonModeli = "iPhone 14"
+        val ekranBoyutu = "6.1 inç"
+        val agirlik = 0.174
+
+        val ulusalGun = "29 Ekim"
+        val tatilGunu = "1 Mayıs"
+        val rezervasyonTarihi = "15.06.2025"
+        val sokakAdi = "İstiklal Caddesi"
+        val otobusHatti = "38"
+        val kalanDakika = 12
+        val takipKodu = "TR1234567890"
+        val kuponSuresi = 30
+        val kuponKodu = "INDIRIM25"
+        val faturaAdresi = "Atatürk Mah. No:10 Kadıköy/İstanbul"
+
+        println(sehir)
+        println(ulke)
+        println(telefon)
+        println(postaKodu)
+        println(email)
+        println(meslek)
+        println(stokMiktari)
+        println(musteriAdi)
+        println(bakiye)
+        println(dogumGunu)
+        println(maas)
+        println(medeniDurum)
+        println(urunYorum)
+
+        println(odemeTarihi)
+        println(odeme)
+        println(siparisAdeti)
+        println(arabaModeli)
+        println(kitapAdi)
+        println(yayinlamaTarihi)
+        println(indirimMiktari)
+        println(odaSayisi)
+        println(enlem)
+        println(boylam)
+        println(urunAdi)
+        println(yemekFiyati)
+
+        println(marka)
+        println(muzikAdi)
+        println(videoSuresi)
+        println(urunPuani)
+        println(resimAdi)
+        println(dosyaFormati)
+        println(renk)
+        println(renkKodu)
+        println(telefonModeli)
+        println(ekranBoyutu)
+        println(agirlik)
+
+        println(ulusalGun)
+        println(tatilGunu)
+        println(rezervasyonTarihi)
+        println(sokakAdi)
+        println(otobusHatti)
+        println(kalanDakika)
+        println(takipKodu)
+        println(kuponSuresi)
+        println(kuponKodu)
+        println(faturaAdresi)
+    }
+
+
+}
